4d3d16025614426e2f18050412da01b66968a2aa
Milton Pagan <milton.pagan1@upr.edu>
2021-04-21 15:24:11 -0400

    Revert "Feature/dispensing mechanism dev"

--- main.c
+++ main.c
@@ -1,49 +1,27 @@
-#include "main.h"
-#include "init.h"
-#include "comms.h"
+#include <msp430.h> 
+
+
+#define SDA BIT0
+#define SCL BIT1
 
 /**
  * main.c
  */
-
-// Global variables
-
-// Current pills in dispenser (string array 8)
-// Current pill counts (int array 8)
-// Reminders array (determine a limit)
-
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	// Scheduling system setup
 
-    // System setup
+	// I2C Setup
+	P4SEL1 |= SDA | SCL;
+	P4SEL0 &= ~SDA & ~SCL;
 
-    init_sched_syst();
-    init_disp_mech();
-    init_comms();
-    init_unused();
+	// Dispensing mechanism setup
 
-    // Disable the GPIO power-on default high-impedance mode to activate
-    // previously configured port settings
-    PM5CTL0 &= ~LOCKLPM5;
 
-    _bis_SR_register(GIE);
+	// Analytics and communication setup
 
-    while (1)
-    {
-        if (finished_rx)
-        {
-            check_params(RX_data);
-            add_alarm();
-            finished_rx = false;
-        }
-        else
-        {
-            _bis_SR_register(LPM0_bits + GIE);
-        }
-    }
+
+	return 0;
 }
-
-//************************************************************************
-// ISRs ******************************************************************
-//************************************************************************
